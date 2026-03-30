5bbc1f7eecbacd6031876ef58452103da05fd29d
leonrdardenaitech <leonrdarden@gmail.com>
2026-03-30 05:32:24 -0400

    fix(wavio): ensure correct imports and styles in main entry point to resolve blank screen

--- src/wavio-main.jsx
+++ src/wavio-main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import WavioApp from './WavioApp'
 import './index.css'
+import './wavio.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
